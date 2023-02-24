a2271a48060ec5ebf619a2ea6f72e192599170c6
bezalelowusu <74635032+Bezalel123@users.noreply.github.com>
2023-02-24 13:21:20 +0000

    Add files via upload

---
+++ Assignment two (6946021).py
@@ -0,0 +1,71 @@
+#this script give details about cars
+import sys
+hello=input('hello welcome to THE WISOM BEHIND, if you want to proceed input yes: ')
+if hello == 'yes' :
+    print('welcome once again')
+else :
+    sys.exit(0) #at this point
+results=input('which type of car do you prefer?: ')
+if results == str('benz c200 4matic') :
+    print('the price is five million dollars, thank you.')
+if results == str('benz c300 4matic') :
+    print('the price is nine million dollars, thank you.')
+if results == str('benz c250 4matic') :
+    print('the price is seven million dollars, thank you.')
+if results == str('benz s500 4matic') :
+    print('the price is ten million dollars, thank you.')
+if results == str('benz s580 4matic') :
+    print('the price is twenty million dollars, thank you.')
+if results == str('benz s350 4matic') :
+    print('the price is twelve million dollars, thank you.')
+if results == 'benz suv' :
+    print('the price is hundred million dollars, thank you.')
+if results == 'toyota corolla s' :
+    print('the price is thirty thousand dollars, thank you.')
+if results == 'toyota corolla le' :
+    print('the price is six thousand dollars, thank you.')
+if results == 'toyota corolla hatch' :
+    print('the price is eight thousand dollars, thank you.')
+if results == 'toyota avante' :
+    print('the price is fifty thousand dollars, thank you.')
+if results == 'toyota elantra' :
+    print('the price is sixty thousand dollars, thank you.')
+if results == 'toyota avensis' :
+    print('the price is six thousand dollars, thank you.')
+if results == 'toyota camry spider' :
+    print('the price is twenty thousand dollars, thank you.')
+if results == 'range rover' :
+    print('the price is ninety thousand dollars, thank you.')
+if results == 'kia' :
+    print('the price is two thousand dollars, thank you.')
+if results == 'bugatti' :
+    print('the price is seven million dollars, thank you.')
+if results == 'chevrolet' :
+    print('the price is eighty thousand dollars, thank you.')
+if results == 'bmw' :
+    print('the price is two million dollars, thank you.')
+if results == 'honda' :
+    print('the price is twelve thousand dollars, thank you.')
+if results == 'hyundai' :
+    print('the price is eleven thousand dollars, thank you.')
+if results == 'vibe' :
+    print('the price is four thousand dollars, thank you.')
+if results == 'tesla' :
+    print('the price is fourty million dollars, thank you.')
+if results == 'bentley' :
+    print('the price is fifty million dollars, thank you.')
+if results == 'rolls royce' :
+    print('the price is three hundred million dollars, thank you.')
+if results == 'tundra' :
+    print('the price is hundred million dollars, thank you.')
+if results == 'porsche' :
+    print('the price is seventy million dollars, thank you.')
+if results == 'lamborghini' :
+    print('the price is two hundred million dollars, thank you.')
+if results == 'ferrari' :
+    print('the price is two hundred million dollars, thank you.')
+if results == 'highlander' :
+    print('the price is four million dollars, thank you.')
+else :
+    sys.exit(0)
+#https://github.com/Bezalel123/Data-structure.git
